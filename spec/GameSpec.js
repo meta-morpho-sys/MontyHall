c104c3356b77748760113bf437794446512e6ffd
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-09-06 21:47:34 +0100

    Introduced Player as SpyObj

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,21 +2,18 @@ describe('Game', function () {
     var game;
 
     it('has three empty boxes at the beginning', function(){
-        game = new Game();
-        expect(game.isBoxEmpty).toBeTruthy();
+        expect(montyHall.isBoxEmpty).toBeTruthy();
     });
 
     it('assigns the selected box to contain the prize', function(){
-        game = new Game();
         spyOn(Math, 'random').and.returnValue(0.5);
         var selectedBox = game.setPrize();
         expect(game.isBoxEmpty(selectedBox)).toBeFalsy();
     });
 
     it('resets the boxes to be empty', function(){
-        game = new Game();
-        playedBoxes = game.setPrize();
-        game.resetBoxes();
-        expect(game.isBoxEmpty).toBeTruthy()
-    })
+        montyHall.setPrize();
+        montyHall.resetBoxes();
+        expect(montyHall.isBoxEmpty).toBeTruthy()
+    });
 });
