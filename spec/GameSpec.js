8c052943ec726ecc384cde02b9daacbea2c67df9
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-09-08 02:17:01 +0100

    Refactoring - setPrize and resetBoxes. Modeling boxes as integers.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,48 +1,61 @@
 describe('Game', function () {
-    var montyHall, player;
+    var host, player;
 
     beforeEach(function() {
-        montyHall = new Game();
+        host = new Game();
     });
 
-    it('has three empty boxes at the beginning', function(){
+    xit('has three empty boxes at the beginning', function(){
         expect(montyHall.isBoxEmpty).toBeTruthy();
     });
 
-    it('assigns the selected box to contain the prize', function(){
+    it('> assigns the selected box to contain the prizeBox', function(){
         spyOn(Math, 'random').and.returnValue(0);
-        var selectedBox = montyHall.setPrize();
-        expect(montyHall.isBoxEmpty(selectedBox)).toBeFalsy();
+        host.setPrize();
+        expect(host.prizeBox).toEqual(0);
     });
 
-    it('resets the boxes to be empty', function(){
-        montyHall.setPrize();
-        montyHall.resetBoxes();
-        expect(montyHall.isBoxEmpty).toBeTruthy()
+    it('> resets the boxes to be empty', function(){
+        host.setPrize();
+        host.resetBoxes();
+        expect(host.prizeBox).toBeNull()
     });
 
-    it('picks a box for the player', function(){
+    xit('> picks a box for the player', function(){
         // return value 0-0.3 --> the 0th element (boxOne)
         spyOn(Math, 'random').and.returnValue(0);
-        montyHall.selectBox();
-        expect(montyHall.playersChoice).toEqual(0)
+        host.selectBox();
+        expect(host.playersChoice).toEqual(0)
     });
 
+    xdescribe('> opens another box', function(){
+        it(">> when the prizeBox box and player's box coincide", function() {
+            spyOn(Math, 'random').and.returnValue(0);
+            var prize = host.setPrize();
 
-    it('opens one of the remaining two boxes', function(){
-        spyOn(Math, 'random').and.returnValue(0);
-        var selectedBox = montyHall.selectBox();
-        alert('players box ' + selectedBox);
+            jasmine.getEnv().allowRespy(true);
+            spyOn(Math, 'random').and.returnValue(0);
+            var playersBox = host.selectBox();
 
-        jasmine.getEnv().allowRespy(true);
-        spyOn(Math, 'random').and.returnValue(0.7);
-        var prize = montyHall.setPrize();
-        alert('prize ' + prize);
+            var optionedBox = host.boxes[0];
+            var openedBox = host.getOpenBox();
 
-        alert(montyHall.boxes);
+            expect(openedBox).toBeGreaterThan(optionedBox);
+            expect(host.getOpenBox()).not.toEqual(playersBox);
+            expect(host.getOpenBox()).not.toEqual(prize);
+        });
 
-        expect(montyHall.openBox()).toEqual('empty2');
-        expect(montyHall.openBox()).not.toEqual(selectedBox);
-        expect(montyHall.openBox()).not.toEqual(prize);
+        it(">> when the prizeBox box and and player's box differ", function(){
+            spyOn(Math, 'random').and.returnValue(0);
+            var prize = host.setPrize();
+
+            jasmine.getEnv().allowRespy(true);
+            spyOn(Math, 'random').and.returnValue(0.7);
+            var selectedBox = host.selectBox();
+
+            expect(host.getOpenBox()).toEqual('empty2');
+            expect(host.getOpenBox()).not.toEqual(selectedBox);
+            expect(host.getOpenBox()).not.toEqual(prize);
+        })
     })
 });
