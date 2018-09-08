33c07f7855823a0a473725ef77aa9f5745181ae1
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-09-08 02:15:04 +0100

    Refactoring - Modeling boxes as integers. setPrize and resetBoxes

--- src/Game.js
+++ src/Game.js
@@ -1,33 +1,34 @@
 function Game(){
-    this.boxOne = 'empty1';
-    this.boxTwo = 'empty2';
-    this.boxThree = 'empty3';
-    this.boxes = [this.boxOne, this.boxTwo, this.boxThree];
+    this.boxes = [0, 1, 2]; // these are my boxes
+    this.prizeBox = null;
     this.playersChoice = null;
 }
-
-Game.prototype.isBoxEmpty = function(box){
-    if (box.startsWith('empty')){
-        console.log('empty');
-        return true
-    } else {
-        console.log('prize');
-        return false;
-    }
-};
+// model "box" as an integer 0,1,2
+// have this.prizeBox be assigned an integer instead of a string
+// now you can get rid of names like 'indexOfThis', 'indexOfThat'
+// have a this.player and make it behave like this.prizeBox (
+// Game.prototype.isBoxEmpty = function(box){
+//     if (this.boxes[box].startsWith('empty')){
+//         console.log('empty');
+//         return true
+//     } else {
+//         console.log('prizeBox');
+//         return false;
+//     }
+// };
 
 Game.prototype.getBoxNumber = function () {
     return Math.floor(Math.random() * this.boxes.length);
 };
-// returns a string
+
+// assigns an integer 0,1 or 2 to the prizeBox instance variable
 Game.prototype.setPrize = function() {
-    return this.prize = this.boxes[this.getBoxNumber()] = 'with prize';
+    var prizeBoxIndex = this.getBoxNumber();
+    return this.prizeBox = this.boxes[prizeBoxIndex];
 };
 
 Game.prototype.resetBoxes = function(){
-    this.boxes.forEach(function(box){
-        box = 'empty'
-    })
+    this.prizeBox = null
 };
 
 Game.prototype.selectBox = function() {
@@ -38,7 +39,7 @@ Game.prototype.selectBox = function() {
 Game.prototype.openBox = function() {
     var that = this;
     function isOpenable(boxValue) {
-        return boxValue!== that.boxes[that.playersChoice] && boxValue !== that.prize;
+        return boxValue!== that.boxes[that.playersChoice] && boxValue !== that.prizeBox;
     }
     return this.boxes.find(isOpenable);
 };
