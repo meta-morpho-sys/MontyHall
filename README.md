4117a3af909fa462086047ee3367ad867a16973f
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-09-08 13:39:34 +0100

    Update

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# MontyHall
+# MontyHall problem 
 
-Exploring probabilities of how reliable human intuition can be.
+From [Wikipedia Monty_Hall_problem](https://en.wikipedia.org/wiki/Monty_Hall_problem)
+
+"...Suppose you're on a game show, and you're given the choice of three doors: Behind one door is a car; behind the others, goats. You pick a door, say No. 1, and the host, who knows what's behind the doors, opens another door, say No. 3, which has a goat. He then says to you, "Do you want to pick door No. 2?" Is it to your advantage to switch your choice?..."
+
+
+###### My program is exploring whether it is more probable to win:
+- if keeping the initial choice or
+- if switching to the box that wasn't opened by Host nor was subject of players's first choice.
+
+How reliable human intuition can be....
--- src/Game.js
+++ src/Game.js
@@ -1,39 +1,50 @@
-function Game(){
-    this.boxes = [0, 1, 2]; // these are my boxes 0, 1, 2
-    this.prizeBox = null;
-    this.playersBox = null;
-}
+// Host has 3 boxes [0, 1, 2] and sets one of them to be with a prize (say box 0).
+// On second turn Host picks one box to be the box chosen by the Player. (say box 2).
+
+// Once Host knows which box was chosen by the player, it opens another box which doesn't contain a prize (say box 1)
+// Player is left with the choice to stick to his initial preference (box 2) or
+// to switch to the other box (box 0).
+// My program is exploring whether it is more probable to win:
+// -- if keeping the initial choice or
+// -- if switching to the box that wasn't opened by Host nor was subject of players's first choice.
+
 // model "box" as an integer 0,1,2
 // have this.prizeBox be assigned an integer instead of a string
 // now you can get rid of names like 'indexOfThis', 'indexOfThat'
 // have a this.player and make it behave like this.prizeBox
 // get rid of boxes array?
 
+function Host(){
+    this.boxes = [0, 1, 2]; // these are my boxes 0, 1, 2
+    this.prizeBox = null;
+    this.playersBox = null;
+}
 
-Game.prototype.getBoxNumber = function () {
-    return Math.floor(Math.random() * this.boxes.length);
-};
-
-// assigns an integer 0,1 or 2 to the prizeBox instance variable
-Game.prototype.setPrize = function() {
+// sets one of the boxes < 0,1 or 2 > to be the prizeBox
+Host.prototype.setPrize = function() {
     var prizeBoxIndex = this.getBoxNumber();
     return this.prizeBox = this.boxes[prizeBoxIndex];
 };
 
-Game.prototype.resetBoxes = function(){
+Host.prototype.resetBoxes = function(){
     this.prizeBox = null;
     this.playersBox = null
 };
 
-Game.prototype.selectPlayersBox = function() {
+// Host acts as player and picks a box
+Host.prototype.getPlayersBox = function() {
   var selectedBoxIndex = this.getBoxNumber();
   return this.playersBox = selectedBoxIndex;
 };
 
-Game.prototype.getOpenBox = function() {
+Host.prototype.getOpenBox = function() {
     var that = this;
     function isOpenable(boxValue) {
         return boxValue!== that.boxes[that.playersBox] && boxValue !== that.prizeBox;
     }
     return this.boxes.find(isOpenable);
+};
+
+Host.prototype.getBoxNumber = function () {
+    return Math.floor(Math.random() * this.boxes.length);
 };
